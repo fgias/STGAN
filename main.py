22253d1eacdc437e69b9fa53ccbc6fb763dd4862
Fotis Giasemis <fotis@fotisgiasemis.com>
2026-02-18 02:54:36 +0200

    Add crypto dataset

--- main.py
+++ main.py
@@ -89,6 +89,12 @@ elif opt['dataset'] == 'ximantis_smooth_3_truncated':
     opt['recent_time'] = 1      # ximantis: 1 hour
     opt['num_feature'] = 1      # length of input feature, density
     opt['time_feature'] = 31       # length of time feature, 24 + 7
+elif opt['dataset'] == 'crypto':
+    opt['timestamp'] = 1 # 1h
+    opt['train_time'] = 300
+    opt['recent_time'] = 1
+    opt['num_feature'] = 1 # Adj
+    opt['time_feature'] = 31 # Length of time feature, 24 + 7
 
 opt['save_path'] = opt['root_path'] + opt['dataset'] + '/checkpoint/'
 opt['data_path'] = opt['root_path'] + opt['dataset'] + '/data/'
