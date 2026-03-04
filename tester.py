09f7ea9e4399e25eac349d7911c1c0b3c51ae4b1
Fotis Giasemis <fotis@fotisgiasemis.com>
2026-03-04 19:39:10 +0200

    Add the tester

--- tester.py
+++ tester.py
@@ -36,23 +36,26 @@ class Tester(object):
 
         self.G.eval()
         self.D.eval()
-        result = torch.zeros((self.loader.time_num, self.loader.node_num, 3))
-        for step, ((recent_data, trend_data, time_feature), sub_graph, real_data, index_t, index_r) in enumerate(
+        result = torch.zeros((self.loader.time_num, self.loader.node_num, 4))
+        for step, ((recent_data, trend_data, time_feature), sub_graph, real_returns, index_t, index_r) in enumerate(
                 self.generator):
             """
             recent_data: (batch_size, time, node_num, num_feature)
             trend_data: (batch_size, time, num_feature)
-            real_data: (batch_size, num_adj, num_feature)
+            real_returns: (batch_size, num_adj, num_feature)
             """
             if self.opt['cuda']:
-                recent_data, trend_data, real_data, sub_graph, time_feature = \
-                    recent_data.cuda(), trend_data.cuda(), real_data.cuda(), sub_graph.cuda(), time_feature.cuda()
+                recent_data, trend_data, real_returns, sub_graph, time_feature = \
+                    recent_data.cuda(), trend_data.cuda(), real_returns.cuda(), sub_graph.cuda(), time_feature.cuda()
 
-            real_sequence = torch.cat([recent_data, real_data.unsqueeze(1)], dim=1)  # (batch_size, time, num_adj, input_size)
-            fake_data = self.G(recent_data, trend_data, sub_graph, time_feature)
+            real_sequence = torch.cat([recent_data, real_returns.unsqueeze(1)], dim=1)
+            predicted_returns = self.G(recent_data, trend_data, sub_graph, time_feature)
 
-            fake_sequence = torch.cat([recent_data, fake_data.unsqueeze(1)], dim=1)
-            mse_loss = torch.pow(fake_data - real_data, 2)
+            fake_sequence = torch.cat([recent_data, predicted_returns.unsqueeze(1)], dim=1)
+            mse_loss = torch.pow(predicted_returns - real_returns, 2)
+            
+            # Direction accuracy: did we predict the right sign?
+            direction_correct = torch.sign(predicted_returns) == torch.sign(real_returns)
 
             real_score_D = self.D(real_sequence, sub_graph, trend_data)
             fake_score_D = self.D(fake_sequence, sub_graph, trend_data)
@@ -60,8 +63,9 @@ class Tester(object):
             batch_size = recent_data.shape[0]
             for b in range(batch_size):
                 result[index_t[b].item(), index_r[b].item(), 0] = torch.mean(mse_loss[b, ]).item()
-                result[index_t[b].item(), index_r[b].item(), 1] = real_score_D[b].item()
-                result[index_t[b].item(), index_r[b].item(), 2] = fake_score_D[b].item()
+                result[index_t[b].item(), index_r[b].item(), 1] = torch.mean(direction_correct[b, ].float()).item()
+                result[index_t[b].item(), index_r[b].item(), 2] = real_score_D[b].item()
+                result[index_t[b].item(), index_r[b].item(), 3] = fake_score_D[b].item()
 
         directory = self.opt['result_path']
         if not os.path.exists(directory):
